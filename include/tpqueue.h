2212c131ec085647782a2ce1036b240f55bde09f
katerinadum <katya05102002@gmail.com>
2021-05-15 19:36:06 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -21,7 +21,8 @@ class TPQueue {
 };
 
 template<typename T>
-TPQueue<T>::TPQueue(int sizeQueue) : size(sizeQueue), begin(0), end(0), count(0) {
+TPQueue<T>::TPQueue(int sizeQueue) : size(sizeQueue), 
+begin(0), end(0), count(0) {
   arr = new T[size + 1];
 }
 template<typename T>
